6f7d259e0beeff8f2559fb7433069fa82fe7d2a0
yme754 <134798911+yme754@users.noreply.github.com>
2025-08-20 11:20:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+import ast
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+movies = pd.read_csv("tmdb_5000_movies.csv")
+credits = pd.read_csv("tmdb_5000_credits.csv")
 
+movies = movies.merge(credits, on='title')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+movies.dropna(inplace=True)
 
+def convert(text):
+    L = []
+    for i in ast.literal_eval(text):
+        L.append(i['name'])
+    return L
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def fetch_director(text):
+    L = []
+    for i in ast.literal_eval(text):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+    return L
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def collapse(L):
+    return [i.replace(" ", "") for i in L]
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(lambda x: [i['name'] for i in ast.literal_eval(x)][0:3])
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies['genres'] = movies['genres'].apply(collapse)
+movies['keywords'] = movies['keywords'].apply(collapse)
+movies['cast'] = movies['cast'].apply(collapse)
+movies['crew'] = movies['crew'].apply(collapse)
+
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+new = movies[['movie_id', 'title', 'tags']]
+new['tags'] = new['tags'].apply(lambda x: " ".join(x))
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(new['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+
+pickle.dump(new.to_dict(), open('movie_dict.pkl', 'wb'))
+pickle.dump(similarity, open('similarity.pkl', 'wb'))
+
+print("Training complete! Saved movie_dict.pkl, similarity.pkl")
